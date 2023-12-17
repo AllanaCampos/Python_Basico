2059f33a063795ab0deeb75a6622b60d5558f9a2
leane <leanesoares212@gmail.com>
2023-12-17 17:10:38 -0300

    Atividade P001

---
+++ Leane/P001/dataFruta/__init__.py
@@ -0,0 +1,6 @@
+from .analiseDados import AnaliseDados
+from .data import Data
+from .listaDatas import ListaDatas
+from .listaNomes import ListaNomes
+from .listaSalarios import ListaSalarios
+from .listaIdade import ListaIdades
